85845facc3680a782700127350bc19c6333f876c
Akshay Chauhan <akshayac1802@gmail.com>
2026-09-16 23:20:19 +0530

    Time: 1 ms (90.09%), Space: 42.4 MB (70.11%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+         if(n==1){
+            return true;
+        }else if(n==0){
+            return false;
+        }
+        while(n!=0){
+            if(n%4!=0 && n!=1){
+                return false;
+            }
+            n = n/4;
+        }
+        return true;
+    }
+}
